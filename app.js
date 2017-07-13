3b97c21e4e6ba2b1a47294951a1440d44c85091b
Chris Chung <hiccupingminor@gmail.com>
2017-07-12 18:44:46 -0700

    Add middleware to authenticate news, debate, and profile

--- app.js
+++ app.js
@@ -162,98 +162,9 @@ passport.use(new LocalStrategy({passReqToCallback : true}, function(req, usernam
 // );
 app.use('/api', apiRoutes);
 
-const Topic = require('./models/topics');
-const Debate = require('./models/debates');
-const Statement = require('./models/statements');
-const User = require('./models/users');
-
 app.use('/', index(passport));
 
-app.get('/debate', function(req, res) {
-    Promise.all([Topic.queryLatest().exec(), Debate.queryAll().exec()])
-        .then(function(promiseResultsArray) {
-            const topic = promiseResultsArray[0];
-            const debates = promiseResultsArray[1];
-
-            var bgImage = "../images/piccool.jpeg", prompt = "Should abortion be legal?";
-
-            if(topic) {
-                bgImage = topic.image;
-                prompt = topic.prompt;
-            }
-
-            const data = {
-                topic: topic,
-                debates: debates,
-                user: req.user,
-            };
-
-            res.render('react_main', { page: 'debate', data: JSON.stringify(data) });
-        }).
-        catch(function(err) {
-            console.log(err);
-            res.status(500);
-        })
-});
-
-app.get('/home', function(req, res) {
-    Topic.queryLatest().exec()
-        .then(function(topic) {
-            return Promise.all([Promise.resolve(topic), Statement.queryTopic(topic._id)]);
-        })
-        .then(function(resultsArr) {
-            const topic = resultsArr[0];
-            const statements = resultsArr[1];
-
-            const data = {
-                topic: topic,
-                statements: statements,
-                user: req.user,
-            };
-
-            res.render('react_main', { page: 'home', data: JSON.stringify(data)});
-        })
-        .catch(function(err) {
-            console.log(err);
-            res.status(500);
-        });
-});
-
-app.get('/profile/:id', function(req, res, next) {
-    User.findById(req.params.id)
-        .exec()
-        .then(function(user) {
-            return Promise.all([user, Topic.queryLatest().exec()]);
-        })
-        .then(function(resultsArray) {
-            const user = resultsArray[0];
-            const topic = resultsArray[1];
-
-            resultsArray.push(Statement.queryByTopicAndUser(topic._id, user._id).exec());
-            resultsArray.push(Debate.queryByTopicAndUser(topic._id, user._id).exec());
-
-            return Promise.all(resultsArray);
-        })
-        .then(function(resultsArray) {
-            const user = resultsArray[0];
-            const statement = resultsArray[2];
-            const debates = resultsArray[3];
-
-            const data = {
-                user: user,
-                statement: statement,
-                debates: debates,
-            };
-
-            res.render('react_main', { page: 'profile', data: JSON.stringify(data)});
-        })
-        .catch(function(err) {
-            console.log(err);
-            res.status(404);
-            next();
-    });
-});
-
+app.use('/', require('./routes/web'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,103 @@
 const router = require('express').Router();
+const passport = require('passport');
 
-router.get('/', function(req, res) {
-    res.render('index');
+const Topic = require('../models/topics');
+const Debate = require('../models/debates');
+const Statement = require('../models/statements');
+const User = require('../models/users');
+
+router.use(function(req, res, next) { // TODO - for some reason couldn't get passport's default middleware to work
+    if(!req.user) {
+        res.redirect('/');
+    }
+    else {
+        return next();
+    }
+});
+
+router.get('/debate', function(req, res) {
+    Promise.all([Topic.queryLatest().exec(), Debate.queryAll().exec()])
+        .then(function(promiseResultsArray) {
+            const topic = promiseResultsArray[0];
+            const debates = promiseResultsArray[1];
+
+            var bgImage = "../images/piccool.jpeg", prompt = "Should abortion be legal?";
+
+            if(topic) {
+                bgImage = topic.image;
+                prompt = topic.prompt;
+            }
+
+            const data = {
+                topic: topic,
+                debates: debates,
+                user: req.user,
+            };
+
+            res.render('react_main', { page: 'debate', data: JSON.stringify(data) });
+        }).
+    catch(function(err) {
+        console.log(err);
+        res.status(500);
+    })
+});
+
+router.get('/home', function(req, res) {
+    Topic.queryLatest().exec()
+        .then(function(topic) {
+            return Promise.all([Promise.resolve(topic), Statement.queryTopic(topic._id)]);
+        })
+        .then(function(resultsArr) {
+            const topic = resultsArr[0];
+            const statements = resultsArr[1];
+
+            const data = {
+                topic: topic,
+                statements: statements,
+                user: req.user,
+            };
+
+            res.render('react_main', { page: 'home', data: JSON.stringify(data)});
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(500);
+        });
+});
+
+router.get('/profile/:id', function(req, res, next) {
+    User.findById(req.params.id)
+        .exec()
+        .then(function(user) {
+            return Promise.all([user, Topic.queryLatest().exec()]);
+        })
+        .then(function(resultsArray) {
+            const user = resultsArray[0];
+            const topic = resultsArray[1];
+
+            resultsArray.push(Statement.queryByTopicAndUser(topic._id, user._id).exec());
+            resultsArray.push(Debate.queryByTopicAndUser(topic._id, user._id).exec());
+
+            return Promise.all(resultsArray);
+        })
+        .then(function(resultsArray) {
+            const user = resultsArray[0];
+            const statement = resultsArray[2];
+            const debates = resultsArray[3];
+
+            const data = {
+                user: user,
+                statement: statement,
+                debates: debates,
+            };
+
+            res.render('react_main', { page: 'profile', data: JSON.stringify(data)});
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(404);
+            next();
+        });
 });
 
 module.exports = router;
