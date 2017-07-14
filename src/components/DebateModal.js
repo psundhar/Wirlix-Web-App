5b36590d96a0ac8a0ebd9b899980f8abadf78014
Chris Chung <hiccupingminor@gmail.com>
2017-07-14 09:46:57 -0700

    Display debate threads in profile

--- src/components/DebateModal.js
+++ src/components/DebateModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DebateModal = ({debate}) => {
 
-    const { statement, challenger, challengee } = debate;
+    const { statement, challenger, challengee, messages = [] } = debate;
 
     return (
         <div id="view-debate" className="modal fade" role="dialog">
@@ -46,48 +46,21 @@ const DebateModal = ({debate}) => {
                                 <p className="time-posted">25m</p>
                             </div>
                         )}
+                        { messages.map(m => {
+                            const isChallenger = m.user == challenger._id;
 
-                        <div className="message-box agr-message">
-                            <div className="username">
-                                <p><a href="profile.html"
-                                      style={{background: "url(/images/pexels-photo-103123.jpeg) center center no-repeat"}}></a>
-                                </p>
-                            </div>
-                            <p className="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Suspendisse varius egestas lacinia. </p>
-                            <p className="time-posted">25m</p>
-                        </div>
-                        <div className="message-box dis-message">
-                            <div className="username">
-                                <p><a href="profile.html"
-                                      style={{background: "url(/images/pexels-photo-103123.jpeg) center center no-repeat"}}></a>
-                                </p>
-                            </div>
-                            <p className="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Suspendisse varius egestas lacinia. </p>
-                            <p className="time-posted">25m</p>
-                        </div>
-                        <div className="message-box agr-message">
-                            <div className="username">
-                                <p><a href="profile.html"
-                                      style={{background: "url(/images/pexels-photo-103123.jpeg) center center no-repeat"}}></a>
-                                </p>
-                            </div>
-                            <p className="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Suspendisse varius egestas lacinia. </p>
-                            <p className="time-posted">25m</p>
-                        </div>
-                        <div className="message-box dis-message">
-                            <div className="username">
-                                <p><a href="profile.html"
-                                      style={{background: "url(/images/pexels-photo-103123.jpeg) center center no-repeat"}}></a>
-                                </p>
-                            </div>
-                            <p className="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Suspendisse varius egestas lacinia. </p>
-                            <p className="time-posted">25m</p>
-                        </div>
-                    </div>
+                            return (
+                                <div className={ "message-box " + ( isChallenger ? "agr-message" : "dis-message" )  }>
+                                    <div className="username">
+                                        <p><a href="profile.html"
+                                              style={{background: "url(/images/pexels-photo-103123.jpeg) center center no-repeat"}}></a>
+                                        </p>
+                                    </div>
+                                    <p className="message">{ m.text }</p>
+                                    <p className="time-posted">25m</p>
+                                </div>
+                            );
+                        }) }
                     <div className="vote-box col-md-12">
                         <div className="col-md-6 col-md-offset-3">
                             <p><i className="fa fa-check"/> Subscribe</p>
@@ -100,6 +73,7 @@ const DebateModal = ({debate}) => {
                 </div>
 
             </div>
+        </div>
         </div>
     )
 };
