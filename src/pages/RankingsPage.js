001d06d5ca530500e642fd72000b01c10fa5241f
Chris Chung <hiccupingminor@gmail.com>
2017-07-18 14:06:41 -0700

    Load coexist rankings

--- src/pages/RankingsPage.js
+++ src/pages/RankingsPage.js
@@ -25,14 +25,23 @@ const RankingsPage = React.createClass({
             return s;
         });
 
-        const factualDebaters = cachedVoteStatements.sort((a,b) => {
+        const factualStatements = [...cachedVoteStatements].filter(s => s.rational > 0).sort((a,b) => {
             if(a.rational > b.rational) {
                 return -1;
             }
             return 1;
         });
 
-        const topFactualDebater = factualDebaters.shift();
+        const emotionalStatements = [...cachedVoteStatements].filter(s => s.emotional > 0).sort((a,b) => {
+            if(a.emotional > b.emotional) {
+                return -1;
+            }
+            return 1;
+        });
+
+        const topFactualStatement = factualStatements.shift();
+        const topEmotionalStatement = emotionalStatements.shift();
+        console.log(emotionalStatements);
 
         return (
             <section className="rankings-section pb4">
@@ -48,23 +57,24 @@ const RankingsPage = React.createClass({
                         <div className="top-ranks">
                             <div className="factual rank-content">
                                 <h2><img src="images/best-debater.png" /> Most Factual Debater</h2>
+                                { factualStatements.length == 0 && (<p className="mt4 center">Waiting for more votes.</p>) }
                                 <div className="rank-container">
-                                    { topFactualDebater && (
+                                    { topFactualStatement && (
                                         <div className="first-place">
-                                            <div className="rank-item"><span className="rank-number">1</span> { topFactualDebater.user.username }</div>
+                                            <div className="rank-item"><span className="rank-number">1</span> { topFactualStatement.user.username }</div>
                                         </div>
                                     )}
                                     <div className="clearfix" style={{minHeight: "520px"}}>
                                         <div className="col col-6 pr1">
                                             <ul>
-                                                { factualDebaters.slice(0,13).map((d, i) => {
+                                                { factualStatements.slice(0,13).map((d, i) => {
                                                     return (<li className="rank-item" key={i}><span className="rank-number">{ i + 2 }</span> { d.user.username }</li>)
                                                 }) }
                                             </ul>
                                         </div>
                                         <div className="col col-6 pl1">
                                             <ul>
-                                                { factualDebaters.slice(13,24).map((d, i) => {
+                                                { factualStatements.slice(13,24).map((d, i) => {
                                                     return (<li className="rank-item" key={i}><span className="rank-number">{ i + 2 }</span> { d.user.username }</li>)
                                                 }) }
                                             </ul>
@@ -83,42 +93,26 @@ const RankingsPage = React.createClass({
                             <div className="emotional rank-content">
                                 <h2><i className="fa fa-hand-peace-o" aria-hidden="true"></i> Most <span className="coexist"><span className="C">C</span><span className="O">O</span><span className="E">E</span><span className="X">X</span><span className="I">I</span><span className="S">S</span><span className="T">T</span><span className="I">I</span><span className="N">N</span><span className="G">G</span></span>
                                     Debater</h2>
+                                { emotionalStatements.length == 0 && (<p className="mt4 center">Waiting for more votes.</p>) }
                                 <div className="rank-container">
-                                    <div className="first-place">
-                                        <div className="rank-item"><span className="rank-number">1</span> John Appleseed</div>
-                                    </div>
-
-                                    <div className="clearfix">
+                                    { topEmotionalStatement && (
+                                        <div className="first-place">
+                                            <div className="rank-item"><span className="rank-number">1</span> { topEmotionalStatement.user.username }</div>
+                                        </div>
+                                    )}
+                                    <div className="clearfix" style={{minHeight: "520px"}}>
                                         <div className="col col-6 pr1">
                                             <ul>
-                                                <li className="rank-item"><span className="rank-number">2</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">3</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">4</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">5</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">6</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">7</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">8</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">9</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">10</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">11</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">12</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">13</span> John Appleseed</li>
+                                                { emotionalStatements.slice(0,13).map((d, i) => {
+                                                    return (<li className="rank-item" key={i}><span className="rank-number">{ i + 2 }</span> { d.user.username }</li>)
+                                                }) }
                                             </ul>
                                         </div>
                                         <div className="col col-6 pl1">
                                             <ul>
-                                                <li className="rank-item"><span className="rank-number">14</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">15</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">16</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">17</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">18</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">19</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">20</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">21</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">22</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">23</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">24</span> John Appleseed</li>
-                                                <li className="rank-item"><span className="rank-number">25</span> John Appleseed</li>
+                                                { emotionalStatements.slice(13,24).map((d, i) => {
+                                                    return (<li className="rank-item" key={i}><span className="rank-number">{ i + 2 }</span> { d.user.username }</li>)
+                                                }) }
                                             </ul>
                                         </div>
                                     </div>
