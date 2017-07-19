b7f7ec25c6d2364047348d44fe6c3aab22beff16
Chris Chung <hiccupingminor@gmail.com>
2017-07-18 18:51:44 -0700

    Writing a new message should update message timestamp

--- controllers/DebatesController.js
+++ controllers/DebatesController.js
@@ -93,6 +93,7 @@ module.exports = {
             }
             if(message) {
                 debate.messages.push(message);
+                debate.updated = Date.now();
             }
 
             return debate.save();
