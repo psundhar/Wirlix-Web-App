c79691247b851ad88ddb5a1f39ef51061d68fcfc
Chris Chung <hiccupingminor@gmail.com>
2017-07-20 10:57:58 -0700

    Add null state message for my debates

--- src/components/MyDebates.js
+++ src/components/MyDebates.js
@@ -2,11 +2,14 @@ import React from 'react';
 import FirstArgumentCard from './FirstArgumentCard';
 
 export default ({debates, userId, handleReplyClick}) => {
+    const myDebates = debates.filter((d) => {
+        return d.challenger._id == userId || d.challengee._id == userId;
+    });
+
     return (<section className="my-debates">
         <div className="container">
-            { debates.filter((d) => {
-                return d.challenger._id == userId || d.challengee._id == userId;
-            }).map((d, i) => {
+            { myDebates.length == 0 && (<p>You aren't active in any debates yet.</p>) }
+            { myDebates.map((d, i) => {
                 return (<FirstArgumentCard handleReplyClick={handleReplyClick} debate={d} challenger={ userId == d.challenger._id } key={i}/>)
             })}
         </div>
