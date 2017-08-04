121730e96c7d4d09cd9ab58081514066c368cfce
Chris Chung <hiccupingminor@gmail.com>
2017-08-03 23:15:19 -0700

    WIP commit

--- src/pages/DebatePage.js
+++ src/pages/DebatePage.js
@@ -224,11 +224,12 @@ const DebatePage = React.createClass({
         <div className="overlay"></div>
         </section>
 
+
         <DebateModal questions={topic.questions} handleEndDebate={this.handleEndDebate} user={user} handleNewMessage={this.handleNewMessage} debate={this.state.debateModal.debate} />
-        { this.state.showEndDebateMessage && (<div className="end-message">
-            <p className="quote">Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.</p>
-            <p className="coexist"><span className="C">C</span><span className="O">O</span><span className="E">E</span><span className="X">X</span><span className="I">I</span><span className="S">S</span><span className="T">T</span></p>
-        </div>) }
+        {/*{ this.state.showEndDebateMessage && (<div className="end-message">*/}
+            {/*<p className="quote">Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.</p>*/}
+            {/*<p className="coexist"><span className="C">C</span><span className="O">O</span><span className="E">E</span><span className="X">X</span><span className="I">I</span><span className="S">S</span><span className="T">T</span></p>*/}
+        {/*</div>) }*/}
 
             </div>)
 
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -315,118 +315,13 @@ const ProfilePage = React.createClass({
 
                 <DebateModal questions={topic.questions} handleEndDebate={ this.handleEndDebate } user={this.state.loggedInUser} handleNewMessage={this.handleNewMessage} debate={ this.state.debateModal.debate }/>
 
-                { this.state.showEndDebateMessage && (<div className="end-message" style={{display: "block", backgroundColor: "white"}}>
-                    <p className="quote" style={{color: "black"}}>Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.</p>
-                    <p className="coexist"><span className="C">C</span><span className="O">O</span><span className="E">E</span><span className="X">X</span><span className="I">I</span><span className="S">S</span><span className="T">T</span></p>
-                </div>) }
+                {/*{ this.state.showEndDebateMessage && (<div className="end-overlay">*/}
+                    {/*<div className="end-message" style={{backgroundColor: "black"}}>*/}
+                    {/*<p className="quote" style={{color: "white"}}>Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.</p>*/}
+                    {/*<p className="coexist"><span className="C">C</span><span className="O">O</span><span className="E">E</span><span className="X">X</span><span className="I">I</span><span className="S">S</span><span className="T">T</span></p>*/}
+                    {/*</div>*/}
+                {/*</div>) }*/}
 
-                <div id="rankings" className="modal fade" role="dialog">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="my-ranking">
-                                <p className="my-info"><a href="profile.html"
-                                                          style={{background: "url(img/north-korea-image.jpg) center center no-repeat"}}></a>
-                                    Priyanka Sundhar</p>
-                            </div>
-                            <div className="my-rank-num">
-                                <p className="my-rank">#434</p>
-                            </div>
-                            <div className="top-ranks">
-                                <div className="factual rank-content">
-                                    <h2><img src="/images/best-debater.png"/> Most Factual Debater</h2>
-                                    <ul className="rank-container col-md-12">
-                                        <div className="first-place">
-                                            <li><span className="rank-number">1</span> John Appleseed</li>
-                                        </div>
-                                        <div className="col-md-6 col-sm-6">
-                                            <li><span className="rank-number">2</span> John Appleseed</li>
-                                            <li><span className="rank-number">3</span> John Appleseed</li>
-                                            <li><span className="rank-number">4</span> John Appleseed</li>
-                                            <li><span className="rank-number">5</span> John Appleseed</li>
-                                            <li><span className="rank-number">6</span> John Appleseed</li>
-                                            <li><span className="rank-number">7</span> John Appleseed</li>
-                                            <li><span className="rank-number">8</span> John Appleseed</li>
-                                            <li><span className="rank-number">9</span> John Appleseed</li>
-                                            <li><span className="rank-number">10</span> John Appleseed</li>
-                                            <li><span className="rank-number">11</span> John Appleseed</li>
-                                            <li><span className="rank-number">12</span> John Appleseed</li>
-                                            <li><span className="rank-number">13</span> John Appleseed</li>
-                                        </div>
-                                        <div className="col-md-6 col-sm-6">
-                                            <li><span className="rank-number">14</span> John Appleseed</li>
-                                            <li><span className="rank-number">15</span> John Appleseed</li>
-                                            <li><span className="rank-number">16</span> John Appleseed</li>
-                                            <li><span className="rank-number">17</span> John Appleseed</li>
-                                            <li><span className="rank-number">18</span> John Appleseed</li>
-                                            <li><span className="rank-number">19</span> John Appleseed</li>
-                                            <li><span className="rank-number">20</span> John Appleseed</li>
-                                            <li><span className="rank-number">21</span> John Appleseed</li>
-                                            <li><span className="rank-number">22</span> John Appleseed</li>
-                                            <li><span className="rank-number">23</span> John Appleseed</li>
-                                            <li><span className="rank-number">24</span> John Appleseed</li>
-                                            <li><span className="rank-number">25</span> John Appleseed</li>
-                                        </div>
-                                    </ul>
-                                    <div className="next-buttons col-md-12 col-sm-12">
-                                        <div className="col-md-6 col-sm-6">
-                                            <button className="back">Back</button>
-                                        </div>
-                                        <div className="col-md-6 col-sm-6">
-                                            <button className="next">Next</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="emotional rank-content">
-                                    <h2><i className="fa fa-hand-peace-o" aria-hidden="true"/> Most <span
-                                        className="coexist"><span className="C">C</span><span
-                                        className="O">O</span><span className="E">E</span><span
-                                        className="X">X</span><span className="I">I</span><span
-                                        className="S">S</span><span className="T">T</span><span
-                                        className="I">I</span><span className="N">N</span><span
-                                        className="G">G</span></span>
-                                        Debater</h2>
-                                    <ul className="rank-container col-md-12">
-                                        <div className="first-place">
-                                            <li><span className="rank-number">1</span> John Appleseed</li>
-                                        </div>
-                                        <div className="col-md-6 col-sm-6">
-                                            <li><span className="rank-number">2</span> John Appleseed</li>
-                                            <li><span className="rank-number">3</span> John Appleseed</li>
-                                            <li><span className="rank-number">4</span> John Appleseed</li>
-                                            <li><span className="rank-number">5</span> John Appleseed</li>
-                                            <li><span className="rank-number">6</span> John Appleseed</li>
-                                            <li><span className="rank-number">7</span> John Appleseed</li>
-                                            <li><span className="rank-number">8</span> John Appleseed</li>
-                                            <li><span className="rank-number">9</span> John Appleseed</li>
-                                            <li><span className="rank-number">10</span> John Appleseed</li>
-                                            <li><span className="rank-number">11</span> John Appleseed</li>
-                                            <li><span className="rank-number">12</span> John Appleseed</li>
-                                            <li><span className="rank-number">13</span> John Appleseed</li>
-                                        </div>
-                                        <div className="col-md-6 col-sm-6">
-                                            <li><span className="rank-number">14</span> John Appleseed</li>
-                                            <li><span className="rank-number">15</span> John Appleseed</li>
-                                            <li><span className="rank-number">16</span> John Appleseed</li>
-                                            <li><span className="rank-number">17</span> John Appleseed</li>
-                                            <li><span className="rank-number">18</span> John Appleseed</li>
-                                            <li><span className="rank-number">19</span> John Appleseed</li>
-                                            <li><span className="rank-number">20</span> John Appleseed</li>
-                                            <li><span className="rank-number">21</span> John Appleseed</li>
-                                            <li><span className="rank-number">22</span> John Appleseed</li>
-                                            <li><span className="rank-number">23</span> John Appleseed</li>
-                                            <li><span className="rank-number">24</span> John Appleseed</li>
-                                            <li><span className="rank-number">25</span> John Appleseed</li>
-                                        </div>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="close-bottom">
-                                <button type="button" className="btn btn-default" data-dismiss="modal"><i
-                                    className="fa fa-times-circle" aria-hidden="true"/></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div id="challenge-conf" className="modal fade" role="dialog">
                     <div className="modal-dialog">
                         <div className="modal-content">
