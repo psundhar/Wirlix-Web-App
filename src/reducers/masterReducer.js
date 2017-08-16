53b9bc60ce08714b3c950a6e842529d218d59d21
Chris Chung <hiccupingminor@gmail.com>
2017-08-16 10:54:13 -0700

    Get submit method working properly

--- src/reducers/masterReducer.js
+++ src/reducers/masterReducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import statements from './statementReducer';
+import user from './userReducer';
+import topic from './topicReducer';
 
 export default combineReducers({
     statements,
+    user,
+    topic
 });
