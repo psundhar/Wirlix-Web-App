1acca00e4820cc95f25597f52a831335600836b8
Pooja <pooja@wirlix.com>
2017-08-21 18:45:55 -0700

    Interchange yes and no positions

--- src/components/ImageDialog.js
+++ src/components/ImageDialog.js
@@ -9,12 +9,13 @@ const ImageDialog = React.createClass({
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <p>Are you sure you want to continue without a profile picture?</p>
-                        <div className="col-xs-6 col-sm-6 col-md-6 cancel">
-                            <button data-dismiss="modal" onClick={ () => handleCancel() }>No</button>
-                        </div>
                         <div className="col-xs-6 col-sm-6 col-md-6 confirm">
                             <button data-dismiss="modal" onClick={ () => handleConfirm() }>Yes</button>
                         </div>
+                        <div className="col-xs-6 col-sm-6 col-md-6 cancel">
+                            <button data-dismiss="modal" onClick={ () => handleCancel() }>No</button>
+                        </div>
+
                     </div>
                 </div>
             </div>
