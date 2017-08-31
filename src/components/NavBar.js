62cc57915d263f1753db5a85ce161416b68836e0
Ranjita Das <ranjita.renee.das@gmail.com>
2017-08-30 19:51:49 -0700

    Updated Styling index and nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -82,17 +82,17 @@ const NavBar = React.createClass({
                         </div>
                         <div className="collapse navbar-collapse center-m" id="bs-example-navbar-collapse-1">
 
-                            <ul className="nav navbar-nav center" style={{fontFamily:"1.3em" , marginLeft: "110px",borderRadius:'15px' }}>
+                            <ul className="nav navbar-nav center" style={{fontFamily:"1.3em", borderRadius:'15px' }}>
                                 <li><Link to="/home"><span style={{color:"white",fontSize: "1.2em", fontFamily: 'Source Code Pro'}}>Live</span></Link></li>
                                 <li><Link to="/debate"><span style={{color:"white",fontSize: "1.2em", fontFamily: 'Source Code Pro'}}>Debate</span></Link></li>
-                                <li><Link to="/rankings"><span style={{color:"white",fontSize: "1.2em", fontFamily: 'Source Code Pro'}}>Ranking</span></Link></li>
+            {/*<li><Link to="/rankings"><span style={{color:"white",fontSize: "1.2em", fontFamily: 'Source Code Pro'}}>Ranking</span></Link></li>*/}    
 {/*
                                 <li><Link to="/about"><span style={{color:"white",fontSize: "1.2em", fontFamily: 'Source Code Pro'}}>About</span></Link></li>
 */}
                             </ul>
-                            <ul className="nav navbar-nav navbar-right">
-                                <li><a className="profile-nav" onClick={this.toggleMenu} style={{background: "url(" + profileImage + ") center center no-repeat" }} >
-                                    { notify && (<div style={{position:'absolute',top:0,left:0,height:"13px",width:"13px",borderRadius:"100px",backgroundColor:"crimson"}}></div>) }
+                            <ul className="nav navbar-nav navbar-right" style={{paddingRight:"20px"}}>
+                                <li><a className="profile-nav" onClick={this.toggleMenu} style={{background: "url(" + profileImage + ") center center no-repeat", }} >
+                                    { notify && (<div style={{position:'absolute',top:0,left:0,height:"20px",width:"20px",borderRadius:"100px",backgroundColor:"crimson"}}></div>) }
                                 </a>
                                     <ul className="dropdown-menu" id="dropdown" style={{display: isOpen ? "inline-block" : "none", background: "white", borderRadius:"15px"}}>
                                         <li><Link to={"/profile/" + user._id}><span style={{color:"#424242", borderBottom: '1px solid grey', fontSize: "1.2em"}}><b>Profile   ></b></span></Link></li>
