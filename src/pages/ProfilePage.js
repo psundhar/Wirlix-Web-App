0de7b1972b5f95126c29f30080cd9d3d7e8e5a79
Pooja <pooja@wirlix.com>
2017-09-01 19:35:37 -0700

    Added new styling for Profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -209,43 +209,26 @@ const ProfilePage = React.createClass({
                 <div className="main-content profile" style={{minHeight:"1400px"}}>
                     <NavBar user={loggedInUser}/>
                     <section className="profile-container">
-                        <div className="container">
-                            <div className="profile-pic col-md-4 col-md-offset-4">
+                    <div className="container gradient" style={{paddingBottom:"50px", borderBottom:"2px solid white"}}>
+                            <div className="col-md-6 profile-pic">
                                 <div className="pic-crop"
                                      style={{background: "url(" + profileImage + ") center center no-repeat"}}></div>
                             </div>
-                        </div>
-                        <div className="container">
-                            <div className="profile-content col-md-8 col-md-offset-2">
-                                <div className="border-bottom border-white clearfix pb3">
-                                    <h2 className="mb0">{ profileName }</h2>
-                                    <h3 className="small italic mb3">@{ profileUser.username }</h3>
-                                    <div className="mb2 col-md-12">
-                                        <EditableBio isEditable={ isMyProfile } handleEdit={handleBioEdit(profileUser)} bio={ profileUser.bio } />
 
-
-                                        {/*
-                                        <TempPopup show={ this.state.showBioEdited } color="white" backgroundColor="crimson"><div className="center bold">Bio Edited</div></TempPopup>
-*/}
-                                    </div>
-                                    <div className="scores">
-                                        <div className="col-md-6">
-                                            <h4>Factual Appeal Rank</h4>
-                                            <p className="p1"><img src="/images/best-debater-w.png" className="m0"/> { factualRank ? factualRank : 'N/A' }</p>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <h4>Emotional Appeal Rank</h4>
-                                            <p className="p1"><img src="/images/peace.png" className="peace m0"/> { emotionalRank ? emotionalRank : 'N/A' }</p>
+                         <div className="col-md-6">
+                                        <div style={{paddingLeft:"0px", marginLeft:"0px", float:"left"}}><h2 className="mb0" >{ profileName }</h2></div>
+                                        <div style={{paddingLeft:"0px", marginLeft:"0px", float:"left", clear:"left", color:"white"}}><h3 className="small italic mb3" >@{ profileUser.username }</h3></div>
+                                        <div className="mb2 col-md-12" style={{paddingLeft:"0px", marginLeft:"0px", float:"left", clear:"left", width:"100%"}}>
+                                                <EditableBio isEditable={ isMyProfile } handleEdit={handleBioEdit(profileUser)} bio={ profileUser.bio } />
                                         </div>
                                     </div>
-                                </div>
-                                <div className="qotd col-md-12 mb4 border-bottom border-white pb3">
+                    </div>
+
+                                <div className="qotd col-md-12 mb4 border-white pb3" style={{paddingBottom:"70px"}}>
                                     <div className="gotd-banner">
-                                        <p style={{fontSize:"1.5em", backgroundColor:"#292C2D"}}> Topic of the Day</p>
-                                        {/*
-                                        <h3 className="mb2">{ topic.prompt }</h3>
-*/}
-                                        <div className="dummyTopic">{ topic.prompt }</div>
+                                        <p style={{fontSize:"1.5em", backgroundColor:"black" ,color:"white"}}> Topic of the Day</p>
+                                        <div className="dummyTopic" style={{color:"black",fontSize:"1.5em", marginBottom:"0px", paddingTop:"30px"}}>{ topic.prompt }</div>
+                                        <div className="profileOpinion col-md-8 col-md-offset-2" style={{borderRadius:"20px"}}>
                                         { isMyProfile && ( <div>
                                             { statements.filter(s=>s.user._id==loggedInUser._id).map((s,i)=>{
                                                 console.log(s);
@@ -274,21 +257,23 @@ const ProfilePage = React.createClass({
 
                                     </div>
                                 </div>
-
+                                </div>
+                    <div className="container" style={{backgroundColor:"white"}}>
                                 { isMyProfile && (
-                                    <div className="col-md-12 border-bottom border-white pb3 mb2">
-                                        <h3 className="large clickable mb3 mt0" onClick={ this.handleMyDebatesClick }>My Debates</h3>
+                                    <div className="col-md-6 border-bottom border-white pb3 mb2" style={{ paddingRight:"30px", paddingTop:"38px"}}>
+                                        <h3 className="large clickable mb3 mt0" style={{paddingBottom:"25px", color:"#C41717"}} onClick={ this.handleMyDebatesClick }><u>My Debates</u></h3>
                                         <MyDebates handleReplyClick={this.handleEnterDebate} debates={ myDebates } user={ profileUser }/>
                                     </div>
                                 )}
 
                                 { !isMyProfile && (<div className="debates col-md-12 border-bottom border-white pb3">
+                                    <h3 className="large clickable mb3 mt0" style={{paddingBottom:"25px", color:"#C41717"}}><u>ACTIVITY</u></h3>
                                     { debates.filter(d => d.challenger._id == profileUser._id||d.challengee._id == profileUser._id)
                                         .map((d, i) => {
                                             return (<FlippableDebateCard handleSubscribeToggle={this.handleSubscribeToggle} key={i} user={ loggedInUser } debate={ d } handleEnterDebate={ this.handleEnterDebate } />)
                                         })}
                                 </div>) }
-                                {isMyProfile && (
+                              {/*  {isMyProfile && (
                                     <div className="logout">
                                         <a href="/logout" className="logout"><img src="/images/logout.png"/></a>
                                     </div>
@@ -297,42 +282,20 @@ const ProfilePage = React.createClass({
                                 {!isMyProfile && ( <div className="challenge">
                                     <p><i className="fa fa-plus-circle" aria-hidden="true" data-toggle="modal"
                                           data-target="#challenge-conf"/></p>
-                                </div> ) }
+                                </div> ) }*/}
 
-                                <div id ="profile-notification"> { isMyProfile && (<ChallengeNotificationsList handleEnterDebate={this.handleEnterDebate} handleAcceptChallenge={this.handleChallengeResponse(true)} handleDeclineChallenge={ this.handleChallengeResponse(false) } user={loggedInUser} />) }
-                                </div>
+                                {/*<div id ="profile-notification"> { isMyProfile && (<ChallengeNotificationsList handleEnterDebate={this.handleEnterDebate} handleAcceptChallenge={this.handleChallengeResponse(true)} handleDeclineChallenge={ this.handleChallengeResponse(false) } user={loggedInUser} />) }
+                                </div>*/}
+                        { isMyProfile && (<div className="col-md-6" style={{borderLeft:"2px solid darkgray"}}>
+                            <div id ="profile-notification"> <ChallengeNotificationsList handleEnterDebate={this.handleEnterDebate} handleAcceptChallenge={this.handleChallengeResponse(true)} handleDeclineChallenge={ this.handleChallengeResponse(false) } user={loggedInUser} />
                             </div>
-                            <div className="profile-content notifications col-md-8 col-md-offset-2">
-                                <h2 className="profile-name">Name goes here</h2>
-                                <div className="scores">
-                                    <div className="col-md-6">
-                                        <p><img src="/images/best-debater-w.png"/> 235</p>
-                                    </div>
-                                    <div className="col-md-6">
-                                        <p><img src="/images/peace.png" className="peace"/> 235</p>
-                                    </div>
-                                </div>
-                                <div className="next-buttons col-md-12 col-sm-12">
-                                    <div className="col-md-6 col-sm-6">
-                                        <button className="back">Back</button>
-                                    </div>
-                                    <div className="col-md-6 col-sm-6">
-                                        <button className="next">Next</button>
-                                    </div>
-                                </div>
-                                <div className="logout">
-                                    <a href="index.html" className="logout"><img src="/images/logout.png"/></a>
-                                </div>
-                                <div className="challenge">
-                                    <i className="fa fa-plus-circle" aria-hidden="true" data-toggle="modal"
-                                       data-target="#challenge-conf"/>
-                                </div>
+                        </div>) }
                             </div>
-                        </div>
                     </section>
                 </div>
+            </div>)}
 
-                <DebateModal handleSubscribeToggle={this.handleSubscribeToggle} questions={topic.questions} handleEndDebate={ this.handleEndDebate } user={loggedInUser} handleNewMessage={this.handleNewMessage} debate={ this.state.modalDebate }/>
+              /*  <DebateModal handleSubscribeToggle={this.handleSubscribeToggle} questions={topic.questions} handleEndDebate={ this.handleEndDebate } user={loggedInUser} handleNewMessage={this.handleNewMessage} debate={ this.state.modalDebate }/>
 
                 { showEndDebateMessage && (<EndDebateOverlay fadeOut={ showEndDebateMessageFadeOut }/>) }
 
@@ -349,9 +312,8 @@ const ProfilePage = React.createClass({
                         </div>
                     </div>
                 </div>
-            </div>
-        )
-    }
+            </div>*/
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
