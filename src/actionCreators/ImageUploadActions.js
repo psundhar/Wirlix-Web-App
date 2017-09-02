91dffdbd6ade052c69ac391db6ba22ccdb74ba4c
Dominic-Pace <dpace@nhpace.net>
2017-09-02 00:53:17 -0400

    Cleaned up code by removing unnecessary import statement

--- src/actionCreators/ImageUploadActions.js
+++ src/actionCreators/ImageUploadActions.js
@@ -1,5 +1,4 @@
-import { IMG_UPLOAD_REQUEST, IMG_UPLOAD_SUCCESS, IMG_UPLOAD_FAILURE,
-  UPDATE_USER_REQUEST, UPDATE_USER_SUCCESS, UPDATE_USER_FAILURE } from '../types/ImageUploadTypes'
+import { IMG_UPLOAD_REQUEST, IMG_UPLOAD_SUCCESS, IMG_UPLOAD_FAILURE } from '../types/ImageUploadTypes'
 
 export const uploadProfileImage = (userId, imgFormData) => {
   const uploadProfileConfig = {
