1d9f9cce9091aa67d646922dc415549bc6f26d01
Dominic-Pace <dpace@nhpace.net>
2017-09-02 23:45:23 -0400

    Fixed an issue where you can't see text in the profile page

--- src/components/MyDebates.js
+++ src/components/MyDebates.js
@@ -4,7 +4,7 @@ import FirstArgumentCard from './FirstArgumentCard';
 export default ({debates, user, handleReplyClick}) => {
     return (<section className="my-debates clearfix">
         <div>
-            { debates.length == 0 && (<p>You aren't active in any debates yet.</p>) }
+            { debates.length == 0 && (<p style={{color: '#000'}}>You aren't active in any debates yet.</p>) }
             { debates.sort((a, b) => {
                 if(a.updated >= b.updated) {
                     return -1;
